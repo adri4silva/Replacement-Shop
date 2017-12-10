553074fa1ef6f21e97ce829bac88970d307c9ddb
adri4silva <adri4silva@gmail.com>
2017-12-10 19:28:08 +0100

    Renamed class 📝

--- views/login_form.py
+++ views/login_form.py
@@ -2,11 +2,10 @@ import gi
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
-from models.select import Select
 
-class Initial_Form(Gtk.Window):
+class Login_Form(Gtk.Window):
     def __init__(self):
-        Gtk.Window.__init__(self, title="Recambios")
+        Gtk.Window.__init__(self, title="Login")
 
         self.external_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.add(self.external_box)
@@ -41,21 +40,10 @@ class Initial_Form(Gtk.Window):
         #self.new_user_button.connect("clicked", self.on_new_user_clicked)
         self.button_box.pack_start(self.new_user_button, False, False, 0)
         self.validate_button = Gtk.Button(label="Entrar")
-        self.validate_button.connect("clicked", self.on_validate_user_clicked)
+        #self.validate_button.connect("clicked", controller.on_validate_user_clicked)
         self.button_box.pack_end(self.validate_button, False, False, 0)
 
 
         self.show_all()
         self.connect("delete_event", Gtk.main_quit)
 
-    def on_validate_user_clicked(self, button):
-        username = self.user_entry.get_text()
-        password = self.pwd_entry.get_text()
-        if (username, password) == Select().check_user(username=username, password=password):
-            self.validation_label.set_markup("<span color='red'>Usuario Correcto</span>")
-        else:
-            self.validation_label.set_markup("<span color='red'>No existe el usuario</span>")
-
-if __name__ == "__main__":
-    Initial_Form()
-    Gtk.main()
