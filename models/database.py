597906624fcb3c840fd76e92343a6aa55e13d809
adri4silva <adri4silva@gmail.com>
2018-02-21 11:40:09 +0100

    Fixed comment errors

--- models/database.py
+++ models/database.py
@@ -2,7 +2,7 @@ import sqlite3 as db
 
 class DataBase(object):
     """ Database class.
-    Connects to the mariaDB database with the credentials.
+    Connects to the sqlite database with the credentials.
     """
     def __init__(self):
         self.db = db.Connection("/Users/adri/Proyectos/PycharmProjects/RecambiosCoche/recambios.sqlite")
