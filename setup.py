139b43f20c28d0cdf430218b304a682d83d050d4
adri4silva <adri4silva@gmail.com>
2018-03-26 01:47:54 +0200

    Build setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='replacement-shop',
+      version='1.0',
+      description='A administration software in Python3 with gtk',
+      author='Adrian Silva',
+      author_email='adri4silva@gmail.com',
+      packages=['models', 'view_controllers', 'images'],
+      scripts=['GestionRecambios'])
